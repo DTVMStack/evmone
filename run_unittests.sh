38d848d0ed05b635c1147c30d25e051499270b75
Outcry <843648230@qq.com>
2026-01-14 12:10:04 +0000

    add run unittests tool in DTVM

---
+++ run_unittests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cmake -S . -B build -DEVMONE_TESTING=ON
+cmake --build build -j16
+
+TEST_LIST_FILE="./UnittestsRunList.txt"
+FILTER_PARAM=""
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+    line_clean=$(echo "$line" | xargs)
+    if [ -n "$line_clean" ]; then
+        if [ -z "$FILTER_PARAM" ]; then
+            FILTER_PARAM="$line_clean"
+        else
+            FILTER_PARAM="$FILTER_PARAM:$line_clean"
+        fi
+    fi
+done < "$TEST_LIST_FILE"
+
+./build/bin/evmone-unittests --gtest_filter="$FILTER_PARAM"
